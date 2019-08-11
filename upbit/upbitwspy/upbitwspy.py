34cf29aa4e22ae25b2cd6fe6920f716c3e48a3bd
hzybzy <hzybzy@gmail.com>
2019-08-11 07:19:19 +0000

    Add elements(orderbook, ticker)

--- upbit/upbitwspy/upbitwspy.py
+++ upbit/upbitwspy/upbitwspy.py
@@ -1,29 +1,72 @@
 import asyncio
 import websockets
+import json
 
+class Ticker:
+    code = ''
+    opening_price = 0.0
+    high_price = 0.0
+    low_price = 0.0
+    trade_price = 0.0
+    trade_volume = 0.0
+    trade_timestamp = 0
+    timestamp = 0 
+    def print_data(self):
+        print(self.code)
+
+class Orderbook:
+    code = ''
+    timestamp = 0
+    units = []
+
+class Orderbook_Unit(object):
+    def __init__(self, ask_price, bid_price, ask_size, bid_size):
+        ask_price = 0.0
+        bid_price = 0.0
+        ask_size = 0.0
+        bid_size = 0.0
 
 class UpbitWebsocket():
-	def __init__(self):
-		self.uri = "wss://api.upbit.com/websocket/v1"
+    def __init__(self):
+        self.uri = "wss://api.upbit.com/websocket/v1"
+        self.ticker = Ticker()
+        self.orderbook = Orderbook()
 
-	def set_type(self, data_type, codes):
-		self.str = '[{"ticket":"hello"},{"type":"%s","codes":["%s"]}]'%(data_type, codes)
-	def run(self):
-		asyncio.get_event_loop().run_until_complete(self.loop())
-		
+    def set_type(self, data_type, codes):
+        self.str = '[{"ticket":"hello"},{"type":"%s","codes":["%s"]}]'%(data_type, codes)
+    def run(self):
+        asyncio.get_event_loop().run_until_complete(self.loop())
 
-	async def loop(self):
-		async with websockets.connect(self.uri) as websocket:
-			await websocket.send(self.str)
+    async def loop(self):
+        async with websockets.connect(self.uri) as websocket:
+            await websocket.send(self.str)
 
-			while True:
-				greeting = await websocket.recv()
-				print(f"< {greeting}")
+            while True:
+                data = await websocket.recv()
+                ret = json.loads(data)
+                print(ret)
+                if(ret['type'] == 'ticker'):
+                    self.ticker.code = ret['code']
+                    self.ticker.opening_price = ret['opening_price']
+                    self.ticker.high_price = ret['high_price']
+                    self.ticker.low_price = ret['low_price']
+                    self.ticker.trade_price = ret['trade_price']
+                    self.ticker.trade_volume = ret['trade_volume']
+                    self.ticker.trade_timestamp = ret['trade_timestamp']
+                    self.ticker.timestamp = ret['timestamp']
+                    self.ticker.print_data()
+                elif(ret['type'] == 'orderbook'):
+                    self.orderbook.code = ret['code']
+                    self.orderbook.timestamp = ret['timestamp']
+                    self.orderbook.units.clear()
+                    for i in range(5):
+                        self.orderbook.units.append(Orderbook_Unit(ret['orderbook_units'][i]['ask_price'], ret['orderbook_units'][i]['bid_price'], ret['orderbook_units'][i]['ask_size'], ret['orderbook_units'][i]['bid_size']))
 
 
 if __name__ == "__main__":
-	upbit = UpbitWebsocket()
-	upbit.set_type("ticker","KRW-BTC")
-	upbit.run()
+    upbit = UpbitWebsocket()
+#    upbit.set_type("ticker","KRW-BTC")
+    upbit.set_type("orderbook","KRW-BTC")
+    upbit.run()
 
 
