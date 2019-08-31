d1a91233e802f8b8408c180bcce321bfd8a45ba2
hzybzy <hzybzy@gmail.com>
2019-08-31 16:27:04 +0900

    add xrp

--- mychart.py
+++ mychart.py
@@ -28,7 +28,9 @@ app.layout = html.Div([
     dcc.Graph(id="my-graph1"),
     dcc.Graph(id="my-graph2"),
     dcc.Graph(id="my-graph3"),
-    dcc.Graph(id="my-graph4")    
+    dcc.Graph(id="my-graph4"),
+    dcc.Graph(id="my-graph5"),
+    dcc.Graph(id="my-graph6")    
     #dcc.Interval(id='graph_update', interval=5000)
 ], className="container")
 
@@ -36,7 +38,9 @@ app.layout = html.Div([
     [Output("my-graph1", 'figure'),
     Output("my-graph2", 'figure'),
     Output("my-graph3", 'figure'),
-    Output("my-graph4", 'figure')],
+    Output("my-graph4", 'figure'),
+    Output("my-graph5", 'figure'),
+    Output("my-graph6", 'figure')],
     [Input('submit-button', 'n_clicks'),
     Input('period', 'value')])
 def graph_update(n_clicks, period):
@@ -58,6 +62,8 @@ def graph_update(n_clicks, period):
     df2 = data['USD2KRW'].resample('5Min').ohlc()
     df3 = data['KRW2USD_ETH'].resample('5Min').ohlc()
     df4 = data['USD2KRW_ETH'].resample('5Min').ohlc()
+    df5 = data['KRW2USD_XRP'].resample('5Min').ohlc()
+    df6 = data['USD2KRW_XRP'].resample('5Min').ohlc()
     
     trace1 = go.Candlestick(x=df1.index,open=df1['open'],high=df1['high'],low=df1['low'],close=df1['close'],
                            increasing={'line': {'color': '#00CC94'}},decreasing={'line': {'color': '#F50030'}})
@@ -66,6 +72,10 @@ def graph_update(n_clicks, period):
     trace3 = go.Candlestick(x=df3.index,open=df3['open'],high=df3['high'],low=df3['low'],close=df3['close'],
                            increasing={'line': {'color': '#00CC94'}},decreasing={'line': {'color': '#F50030'}})
     trace4 = go.Candlestick(x=df4.index,open=df4['open'],high=df4['high'],low=df4['low'],close=df4['close'],
+                           increasing={'line': {'color': '#00CC94'}},decreasing={'line': {'color': '#F50030'}})                       
+    trace5 = go.Candlestick(x=df5.index,open=df5['open'],high=df5['high'],low=df5['low'],close=df5['close'],
+                           increasing={'line': {'color': '#00CC94'}},decreasing={'line': {'color': '#F50030'}})
+    trace6 = go.Candlestick(x=df6.index,open=df6['open'],high=df6['high'],low=df6['low'],close=df6['close'],
                            increasing={'line': {'color': '#00CC94'}},decreasing={'line': {'color': '#F50030'}})                       
     return {'data': [trace1], 'layout': go.Layout(title=f"KRW2USD",\
             shapes = [dict(x0=0, x1=1, y0=KRW2USD, y1=KRW2USD, xref='paper', yref='y', line_width=2, line_color="rgb(200,0,0)")],\
@@ -77,6 +87,12 @@ def graph_update(n_clicks, period):
             shapes = [dict(x0=0, x1=1, y0=KRW2USD, y1=KRW2USD, xref='paper', yref='y', line_width=2, line_color="rgb(200,0,0)")],\
             annotations=[dict(x=0, y=KRW2USD-0.1, xref='paper', yref='y', showarrow=False, xanchor='left', text='%.2f'%KRW2USD)])},\
             {'data': [trace4], 'layout': go.Layout(title=f"USD2KRW_ETH",\
+            shapes = [dict(x0=0, x1=1, y0=USD2KRW, y1=USD2KRW, xref='paper', yref='y', line_width=2, line_color="rgb(0,0,200)")],\
+            annotations=[dict(x=0, y=USD2KRW+0.1, xref='paper', yref='y', showarrow=False, xanchor='left', text='%.2f'%USD2KRW)])},\
+            {'data': [trace5], 'layout': go.Layout(title=f"KRW2USD_XRP",\
+            shapes = [dict(x0=0, x1=1, y0=KRW2USD, y1=KRW2USD, xref='paper', yref='y', line_width=2, line_color="rgb(200,0,0)")],\
+            annotations=[dict(x=0, y=KRW2USD-0.1, xref='paper', yref='y', showarrow=False, xanchor='left', text='%.2f'%KRW2USD)])},\
+            {'data': [trace6], 'layout': go.Layout(title=f"USD2KRW_XRP",\
             shapes = [dict(x0=0, x1=1, y0=USD2KRW, y1=USD2KRW, xref='paper', yref='y', line_width=2, line_color="rgb(0,0,200)")],\
             annotations=[dict(x=0, y=USD2KRW+0.1, xref='paper', yref='y', showarrow=False, xanchor='left', text='%.2f'%USD2KRW)])}
     
